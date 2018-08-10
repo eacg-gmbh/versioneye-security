5cf983f182cc05d30fef9c9a56bdf3c74bbd066a
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-08-10 13:17:18 +0200

    added debug ouput

--- setcreds.sh
+++ setcreds.sh
@@ -14,4 +14,4 @@ if [ -z "$SECRETS_BUCKET_NAME" ]; then
 fi
 
 # Load the S3 secrets file contents into the environment variables
-eval $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/credentials.txt - | sed 's/^/export /')
+eval $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/credentials.txt - | /bin/sed 's/^/export /')

9ea91129d54c40baa095d64160bd435a6a6e2422
Dr. Stefan Pokorny <stefan.pokorny@eacg.de>
2019-01-30 11:41:59 +0100

    refactored nvd processing

--- sec_crawl.sh
+++ sec_crawl.sh
@@ -3,4 +3,4 @@
 cd /app
 export PATH=/usr/local/bundle/bin:/usr/local/sbin:/usr/local/bin:/bin:$SPATH
 source /app/setcreds.sh
-bundle exec rake versioneye:crawl_nvd >> /dev/stdout 2>&1
+bundle exec rake versioneye:crawl_nvd $1 >> /dev/stdout 2>&1
